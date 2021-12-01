501b55d4f932b8ab0b709c0f072fef839dad8a1c
Arthur Yan <arthuryan08@gmail.com>
2021-11-30 21:00:20 -0300

    Custom TabBar details done
    avatar photo and appointments icon

--- src/components/CustomTabBar.js
+++ src/components/CustomTabBar.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import styled from 'styled-components/native';
+
+import {UserContext} from '../contexts/UserContext';
 
 import HomeIcon from '../assets/home.svg';
 import SearchIcon from '../assets/search.svg';
@@ -17,24 +19,69 @@ const TabItem = styled.TouchableOpacity`
   justify-content: center;
   align-items: center;
 `;
+const TabItemCenter = styled.TouchableOpacity`
+  width: 70px;
+  height: 70px;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffffff;
+  border-radius: 35px;
+  border: 3px solid #4eadbe;
+  margin-top: -20px;
+`;
+const AvatarIcon = styled.Image`
+  width: 24px;
+  height: 24px;
+  border-radius: 12px;
+`;
 
-export default () => {
+export default ({state, navigation}) => {
+  const {state: user} = useContext(UserContext);
+
+  const goTo = screenName => {
+    navigation.navigate(screenName);
+  };
+
   return (
     <TabArea>
-      <TabItem>
-        <HomeIcon width="24" height="24" fill="#FFFFFF" />
+      <TabItem onPress={() => goTo('Home')}>
+        <HomeIcon
+          style={{opacity: state.index === 0 ? 1 : 0.6}}
+          width="24"
+          height="24"
+          fill="#FFFFFF"
+        />
       </TabItem>
-      <TabItem>
-        <SearchIcon width="24" height="24" fill="#FFFFFF" />
+      <TabItem onPress={() => goTo('Search')}>
+        <SearchIcon
+          style={{opacity: state.index === 1 ? 1 : 0.6}}
+          width="24"
+          height="24"
+          fill="#FFFFFF"
+        />
       </TabItem>
-      <TabItem>
-        <TodayIcon width="24" height="24" fill="#FFFFFF" />
+      <TabItemCenter onPress={() => goTo('Appointments')}>
+        <TodayIcon width="32" height="32" fill="#4eadbe" />
+      </TabItemCenter>
+      <TabItem onPress={() => goTo('Favorites')}>
+        <FavoriteIcon
+          style={{opacity: state.index === 3 ? 1 : 0.6}}
+          width="24"
+          height="24"
+          fill="#FFFFFF"
+        />
       </TabItem>
-      <TabItem>
-        <FavoriteIcon width="24" height="24" fill="#FFFFFF" />
-      </TabItem>
-      <TabItem>
-        <AccountIcon width="24" height="24" fill="#FFFFFF" />
+      <TabItem onPress={() => goTo('Profile')}>
+        {user.avatar != '' ? (
+          <AvatarIcon source={{uri: user.avatar}} />
+        ) : (
+          <AccountIcon
+            style={{opacity: state.index === 4 ? 1 : 0.6}}
+            width="24"
+            height="24"
+            fill="#FFFFFF"
+          />
+        )}
       </TabItem>
     </TabArea>
   );
